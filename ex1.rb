722c749519a19e8ff9b927797ec37de12b87b9d0
Sandeep kumar <sandeep_kumar57@optum.com>
2016-11-28 03:35:15 -0600

    Automation Tesing ex1 additional

--- ex1.rb
+++ ex1.rb
@@ -4,3 +4,33 @@ puts "Hello World"
 puts "THis is a new way of learning"
 puts "This code would have been like this" #Hello this is code
 puts "this is fun"
+puts "I will count now my chickens"
+puts "hens", 25 + 30 / 6 
+puts "Rooster", 100-25*3%4
+puts "Now i will count the eggs"
+puts 3+4-1*5%1
+puts  "what is 3+2?", 3+2
+
+
+cars=1000
+
+drivers=300
+
+spaceincar=4
+
+passenger=5000
+
+carsnotdriven=cars-drivers
+
+carsdriven=drivers
+
+carpool=carsdriven*spaceincar
+
+averagepassenger=passenger/carsdriven
+
+puts " THere are #{cars} availabe"
+puts " there are drivers #{drivers} availble"
+puts " there wiell be cars #{carsnotdriven} empty today"
+puts "there are carpool #{carpool} today"
+
+
